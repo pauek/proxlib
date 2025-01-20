01c830b70fe633cc7570f027351dfe05c0bb1690
Pau Fernández <pau.fernandez@upc.edu>
2025-01-20 19:11:42 +0100

    Directorio vendor + bintree.hh limpio

---
+++ bintree.hh
@@ -0,0 +1,97 @@
+#ifndef BINTREE_HH
+#define BINTREE_HH
+
+#include <cassert>
+
+/**
+ * @file BinTree.hh
+ * @class BinTree
+ * @brief A class representing binary trees
+ */
+template <typename T>
+class BinTree {
+   private:
+	/**
+	 * @brief Struct that holds the node's information
+	 */
+	struct Node_ {
+		T				  x;
+		shared_ptr<Node_> left;
+		shared_ptr<Node_> right;
+
+		Node_(const T& x, shared_ptr<Node_> left, shared_ptr<Node_> right)
+			: x(x), left(left), right(right) {}
+	};
+
+	/**
+	 * @brief Pointer to the node of the tree
+	 */
+	shared_ptr<Node_> pnode_;
+
+	/**
+	 * @brief Constructs a tree from a node pointer.
+	 */
+	BinTree(shared_ptr<Node_> pnode) : pnode_(pnode) {}
+
+   public:
+	/**
+	 * @brief Constructs an empty tree. Θ(1).
+	 */
+	BinTree() : pnode_(nullptr) {}
+
+    /**
+     * @brief Constructs a tree as a copy of another tree. Θ(1).
+     */
+	BinTree(const BinTree& t) { pnode_ = t.pnode_; }
+
+    /**
+     * @brief Assigns the tree t to this tree. Θ(1).
+     */
+	BinTree& operator=(const BinTree& t) {
+		pnode_ = t.pnode_;
+		return *this;
+	}
+
+	/**
+	 * @brief Constructs a tree with a value x and no subtrees. Θ(1).
+	 */
+	explicit BinTree(const T& x) { pnode_ = make_shared<Node_>(x, nullptr, nullptr); }
+
+	/**
+	 * @brief Constructs a tree with a value x and two subtrees left and right. Θ(1).
+	 */
+	explicit BinTree(const T& x, const BinTree& left, const BinTree& right) {
+		pnode_ = make_shared<Node_>(x, left.pnode_, right.pnode_);
+	}
+
+	/**
+	 * @brief Tells if this tree is empty. Θ(1).
+	 */
+	bool empty() const { return pnode_ == nullptr; }
+
+	/**
+	 * @brief Returns the left subtree of this tree (cannot be empty). Θ(1).
+	 */
+	BinTree left() const {
+		assert(not empty());
+		return BinTree(pnode_->left);
+	}
+
+	/**
+	 * @brief Returns the right subtree of this tree (cannot be empty). Θ(1).
+	 */
+	BinTree right() const {
+		assert(not empty());
+		return BinTree(pnode_->right);
+	}
+
+	/**
+	 * @brief Returns the value of this tree (cannot be empty). Θ(1).
+	 */
+	const T& value() const {
+		assert(not empty());
+		return pnode_->x;
+	}
+};
+
+#endif
